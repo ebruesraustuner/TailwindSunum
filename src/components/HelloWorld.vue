a6fb5df300a432a5f2ce4e7ab0ac9159950554f2
Ebru Esra <ebru.ustuner@adesso.com.tr>
2021-09-18 18:37:39 +0300

    colros changed

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,8 +1,8 @@
 
 <template>
-<div>
+<div class="h-screen bg-gradient-to-b from-white via-blue-400 to-purple-900 flex justify-center items-center">
 
-  <h1 class="lg: text-xl">Plus Ultra</h1>
+  <h1 class="lg: text-8xl text-white font-bold">Tailwind Time ! </h1>
 </div>
 </template>
 
